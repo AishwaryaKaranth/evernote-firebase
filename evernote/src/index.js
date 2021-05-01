f476ff515f31f7ec81372386c939c384486fc6e9
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2021-05-01 09:00:19 +0530

    changed firebase config

--- evernote/src/index.js
+++ evernote/src/index.js
@@ -4,10 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCfYmFvdzwHYd4x_eb7hM-Axqltn582TJ8",
+  authDomain: "evernote-ec1a9.firebaseapp.com",
+  projectId: "evernote-ec1a9",
+  storageBucket: "evernote-ec1a9.appspot.com",
+  messagingSenderId: "95352223441",
+  appId: "1:95352223441:web:1975dab367f9aa710337f0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
   document.getElementById('root')
 );
 
