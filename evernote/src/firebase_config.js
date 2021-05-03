97d2bc91264edb4c51ff1a37bae151da7d28e30f
Aishwarya Karanth <35144851+AishwaryaKaranth@users.noreply.github.com>
2021-05-03 19:32:46 +0530

    Update firebase_config.js

--- evernote/src/firebase_config.js
+++ evernote/src/firebase_config.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCfYmFvdzwHYd4x_eb7hM-Axqltn582TJ8",
+    apiKey: "",
     authDomain: "evernote-ec1a9.firebaseapp.com",
     projectId: "evernote-ec1a9",
     storageBucket: "evernote-ec1a9.appspot.com",
     messagingSenderId: "95352223441",
-    appId: "1:95352223441:web:1975dab367f9aa710337f0"
+    appId: ""
   };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
